16c359f2f2a1d8de538a22bcedcd5af805168268
Erojiraiyasenin <avaneeshritesh4@gmail.com>
2026-04-06 19:57:07 +0400

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,12 @@
+const config = {
+  mc: {
+    host: 'donut',
+    port: 25565,
+    username: 'YOUR_EMAIL@gmail.com',
+    auth: 'microsoft'
+  },
+  discord: {
+    token: 'MTQ5MDczNzY5NzIyMzA4MjIyNA.GzHCwA.LZdFuWLWfKqWTtzrcYrJRWDWvy1dC6fmDrux24',
+    channelId: '1490740862542675988'
+  }
+};
